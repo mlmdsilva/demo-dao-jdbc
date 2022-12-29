6294f0633c1ebce34a70a526771646fe42cc17b6
Mario Luis <mario.lms@hotmail.com>
2022-12-29 12:40:58 -0300

    Seller entity class

--- src/application/TestProgram.java
+++ src/application/TestProgram.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Departament;
+import model.entities.Seller;
 
 public class TestProgram {
 
@@ -8,7 +11,8 @@ public class TestProgram {
 		
 		Departament obj = new Departament(1,"Books");
 		System.out.println(obj);
-
+		Seller seller = new Seller(21,"Bob","bob@gmai.com",new Date(),3000.00,obj);
+		System.out.println(seller);
 	}
 
 }
