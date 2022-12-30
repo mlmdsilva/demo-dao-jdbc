b0113d2c905b00488887857eca9bb572704614c8
Mario Luis <mario.lms@hotmail.com>
2022-12-29 23:04:51 -0300

    DepartmentDao implementation and test

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -3,6 +3,7 @@ package model.dao;
 import java.util.List;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 import model.entities.Seller;
 
@@ -10,6 +11,10 @@ public class DaoFactory {
 	
 	public static SellerDao createSellerDao() {
 		return new SellerDaoJDBC(DB.getConnection());
+	}
+	
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
 	}
 	
 
